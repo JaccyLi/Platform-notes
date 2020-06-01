eeae33ab530a16d64eac7738d2a1066937e65639
suo.li <suo.li@microfun.com>
2020-06-01 11:35:38 +0800

    update

--- codes_hadoop/python_example/map-example.py
+++ codes_hadoop/python_example/map-example.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python
 import re
 import sys
+
+ytList=[]
 for line in sys.stdin:
     val = line.strip()
     (year, temp, q) = (val[15:19], val[87:92], val[92:93])
     if (temp != "+9999" and re.match("[01459]", q)):
-        print "%s\t%s" % (year, temp)
+        mid = year + "    " + temp
+        ytList.append(mid)
+ytList.sort()
+#print(ytList)
+
+for item in ytList:
+    el = item.strip()
+    (y, t) = (el[0:4], el[8:13])
+    print("{}\t{}".format(y, t))
